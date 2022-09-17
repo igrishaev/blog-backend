7636fddd0a5a8bec6a037195101876cc70d9b954
Ivan Grishaev <ivan@grishaev.me>
2022-09-17 13:33:15 +0300

    updated

--- src/blog_backend/cors.clj
+++ src/blog_backend/cors.clj
@@ -1,9 +1,11 @@
-(ns blog-backend.cors)
+(ns blog-backend.cors
+  "https://grishaev.me/cors/")
 
+(def ORIGIN "https://grishaev.me")
 
 (defn handle-cors [_request]
   {:status 200
    :headers
-   {"Access-Control-Allow-Origin" "https://grishaev.me"
+   {"Access-Control-Allow-Origin" ORIGIN
     "Access-Control-Allow-Methods" "POST"
     "Content-Type" "application/json; charset=utf-8"}})
